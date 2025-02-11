348deec52334e4fa7b58130f07c429d07bedaf9d
Deep Kayastha <deepkayastha6890@gmail.com>
2025-02-11 20:14:40 +0530

    Update index.js

--- ecom_js/index.js
+++ ecom_js/index.js
@@ -2,7 +2,15 @@
 /*********************************************Header Js Starts****************************************************/
 var pop_up_ref = true;
 function onclick_pop() {
-
+    var header_toggle_value = document.querySelector(".responsive_header_option");
+    if (header_icon_value == true) {
+        header_toggle_value.style.display = "flex";
+        header_icon_value = false;
+    }
+    else {
+        header_toggle_value.style.display = "none";
+        header_icon_value = true;
+    }
 }
 
 var header_icon_value = true;
